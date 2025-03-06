28de94e46705b8a3416e9dccb54b644962a418ed
rokon <rokonrabbi21@gmail.com>
2025-03-07 02:26:52 +0600

    chore:database connect with define require spring dependencies

--- backend/src/main/java/com/volunteeringPlatform/volunteeringPlatformBackend/controller/controller.java
+++ backend/src/main/java/com/volunteeringPlatform/volunteeringPlatformBackend/controller/controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
-    @GetMapping("/")
+    @GetMapping("/hello")
    public String getHello() {
        return "Hello World";
    }
