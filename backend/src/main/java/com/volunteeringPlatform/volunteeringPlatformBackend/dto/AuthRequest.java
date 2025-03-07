41dc82c2984cba0e70bb8b5f997711b2201f9482
rokon <rokonrabbi21@gmail.com>
2025-03-08 00:49:31 +0600

    feat:add AuthRequest,AuthResponse dto and CustomUserDetailsService

---
+++ backend/src/main/java/com/volunteeringPlatform/volunteeringPlatformBackend/dto/AuthRequest.java
@@ -0,0 +1,13 @@
+package com.volunteeringPlatform.volunteeringPlatformBackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+    private String email;
+    private String password;
+}
